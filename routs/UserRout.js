86e767890305502f5c7190fcc5c0e5b14e1619a9
Sameer Shekh <samir.sheikh0012@gmail.com>
2021-12-12 13:37:58 +0530

    Login rout

--- routs/UserRout.js
+++ routs/UserRout.js
@@ -12,6 +12,7 @@ const token_key = process.env.TOKEN_KEY;
 //include user models
 
 const User =  require('./../Models/UserModel');
+const bcrypt = require('bcryptjs/dist/bcrypt');
 
 
 //middlewear setup
@@ -31,7 +32,7 @@ router.get('/', (req , res) => {
     })
 })
 
-//defaut raout
+//Register raout
 //method = Post
 //access = pulic
 
@@ -85,7 +86,7 @@ const errors = validationResult(req);
     //if user exists
     if(user){
         return res.status(400).json({
-            status:true,
+            status:false,
             massage:"User already exist",
             error: {
                 email:"Email already exists"
@@ -97,18 +98,48 @@ const errors = validationResult(req);
          let salt = bcypt.genSaltSync(10);
          let hashedPassword = bcypt.hashSync(req.body.password, salt);
 
-         return res.status(200).json({
-            status:true,
-            massage:"User Registration Sucessfully",
-            password:hashedPassword
-           })
+         //Save user in data base
+         const newUser = new User({
+             username:req.body.username,
+             email:req.body.email,
+             password:hashedPassword
+         })
 
-         }
+         //save users
+         newUser.save().then(user => {
+             console.log(user)
+
+            return res.status(200).json({
+                status:true,
+                massage:"User Registration Sucessfully",
+                user:{
+                    id:user._id,
+                    username:user.username,
+                    email:user.email,
+
+
+                }
+               })
+    
+
+         }).catch(error => {
+        
+            return res.status(500).json({
+                status:false,
+                massage:"Database Error",
+                error:{
+                    db_error:"some error in database"
+                }
+              
+        
+            })
+
+         }) }
        
 
     }).catch(error => {
         return res.status(500).json({
-            status:true,
+            status:false,
             massage:"Database Error",
             error:{
                 db_error:"some error in database"
@@ -120,6 +151,96 @@ const errors = validationResult(req);
     })
 
    
+});
+
+//Login raout
+//method = Post
+//access = pulic
+
+router.post("/login",
+[
+    check("email").isEmail().normalizeEmail().withMessage("Please Enter Your email").trim().escape(),
+    check("password").not().isEmpty().withMessage("Please Enter Your Password").trim().escape(),
+],
+ (req , res) => {
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()){
+       
+        let error ={};
+        for(index=0; index < errors.array().length; index++){
+            error = {
+                ...error,
+                [errors.array()[index].param]:errors.array()[index].msg
+            }
+        } 
+        return res.status(400).json({
+            status:false,
+            massage:"Form Validation Error..",
+            error:error
+    
+        })
+}
+      //check email is exist or not
+      User.findOne({email: req.body.email}).then(user =>{
+
+        //if user is not exists
+        if(!user){
+            return res.status(400).json({
+                status:false,
+                massage:"User not exist",
+                error: {
+                    email:"Email not exists"
+                }
+              })
+             }else{
+    
+                  //password matched or not
+
+                let isPasswordMatch = bcypt.compareSync(req.body.password,user.password)
+                if(!isPasswordMatch){
+
+                    return res.status(400).json({
+                        status:false,
+                        massage:"Password not Matched ",
+                        error: {
+                            email:"Password not Matched"
+                        }
+                      })
+
+                }
+                return res.status(200).json({
+                    status:true,
+                    massage:"User Login Sucessfully",
+                    user:{
+                        id:user._id,
+                        username:user.username,
+                        email:user.email,
+
+
+                    }
+                   })
+             
+     }
+     }).catch(error => {
+            return res.status(500).json({
+                status:false,
+                massage:"Database Error",
+                error:{
+                    db_error:"some error in database"
+                }
+              
+        
+            })
+    
+        })
+    
+
+
+   
 })
+
+
+
 
 module.exports= router;
